3f9b314dbbcfdc2e220472acf96873dfb36f68b0
Hritik Tanwar <137378507+HritikTanwar@users.noreply.github.com>
2023-08-05 19:55:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -53,6 +53,159 @@ gsap.from(".about-us img, .about-us-text",{
 })
 
 
+
+
+var FirstImage = document.querySelector('.image1')
+var SecondImage = document.querySelector('.image2')
+var ThirdImage = document.querySelector('.image3')
+var FourthImage = document.querySelector('.image4')
+var Images = document.getElementsByClassName('images')
+var ItemPlace = document.getElementsByClassName('item')
+var FirstItem = document.querySelector('.item1')
+var SecondItem = document.querySelector('.item2')
+var ThirdItem = document.querySelector('.item3')
+var calculateButton = document.querySelector('button');
+var sumDisplay = document.querySelector('p');
+
+let NewUser = document.querySelector('.new-user-info')
+let FirstPage = document.querySelector('.first-page')
+let selected = null
+let firstItemPopulated = false;
+let secondItemPopulated = false;
+let thirdItemPopulated = false;
+
+const form = document.getElementById('form-one')
+const UserEmail = document.getElementsByClassName('user-email')
+const UserPassword = document.getElementsByClassName('user-pass')
+const ErrorMsg = document.getElementById('error-msg')
+const SignUpBtn = document.getElementById('sign-up-btn')
+const BackBtn = document.querySelector('.backbtn')
+
+
+
+
+
+
+//---------------------------CODE FOR CREATING MEAL BOX-----------------
+
+for (image of Images){
+    image.addEventListener('dragstart', (e)=>{
+        console.log("started")
+        selected = e.target
+    })
+}
+
+
+// EVENTLISTENER FOR FIRST ITEM
+    FirstItem.addEventListener('dragend', ()=>{
+        console.log("ended")
+     })
+
+    FirstItem.addEventListener('dragover', (e)=>{
+        e.preventDefault()
+        console.log("over")
+    })
+    FirstItem.addEventListener('drop', (e)=>{
+        e.preventDefault()
+        if(!firstItemPopulated && selected !== null){
+            const clone = selected.cloneNode(true); // Clone the selected image
+             FirstItem.appendChild(clone);
+             selected = null;
+             firstItemPopulated = true
+            console.log("ended")
+        }
+    })
+
+
+
+
+// EVENTLISTENER FOR SECOND ITEM
+    SecondItem.addEventListener('dragover', (e)=>{
+        e.preventDefault()
+        console.log("OVER")
+    })
+    SecondItem.addEventListener('dragend', ()=>{
+        console.log("End")
+    })
+    SecondItem.addEventListener('drop', (e)=>{
+        e.preventDefault()
+        if(!secondItemPopulated && selected !== null){
+            const clone = selected.cloneNode(true); // Clone the selected image
+            SecondItem.appendChild(clone);
+            selected = null;
+            secondItemPopulated = true
+            console.log("ended")
+        }
+    })    
+
+
+// EVENTLISTENER FOR THIRD ITEM
+    ThirdItem.addEventListener('dragover', (e)=>{
+        e.preventDefault()
+        console.log("OVER")
+    })
+    ThirdItem.addEventListener('dragend', ()=>{
+        console.log("End")
+    })
+    ThirdItem.addEventListener('drop', (e)=>{
+        e.preventDefault()
+        if(!thirdItemPopulated && selected !== null){
+            const clone = selected.cloneNode(true); // Clone the selected image
+            ThirdItem.appendChild(clone);
+            selected = null;
+            thirdItemPopulated = true
+            console.log("ended")
+        }
+    })
+
+
+
+
+    calculateButton.addEventListener('click', () => {
+        let sum = 0;
+        let finalSum = 0
+        const rightDivs = document.querySelectorAll('.item');
+        rightDivs.forEach((div) => {
+            const image = div.querySelector('.images');
+            if (image) {
+                const value = parseInt(image.getAttribute('data-value'));
+                sum += value
+                finalSum = Math.floor(sum - (sum * 4.5/100))
+            }
+        });
+        sumDisplay.textContent = `Your Total: Rs ${finalSum}/-`;
+    });
+//----------------------------------------------------------------------
+
+//------------------CODE FOR LOGIN VALIDATIONS!-------------------------
+
+form.addEventListener('submit', function (e){
+    let message = []
+    if(UserPassword.value.length <= 6){
+        console.log("done")
+        message.push("password must be more than 6 character long.")
+    }
+    if(message.length > 0){
+        e.preventDefault()
+        ErrorMsg.textContent = message
+    }
+})
+
+function signUp(){
+    console.log("clicked")
+    FirstPage.style.display = "none"
+    NewUser.style.display = "block"
+}
+
+function backbtn(){
+    console.log("clicked")
+    FirstPage.style.display = "block"
+    NewUser.style.display = "none"
+}
+//----------------------------------------------------------------------
+
+
+//                  CODE FOR HAMBURGER MENU!
 const hamburger = document.querySelector('.hamburger')
         const Links = document.querySelector('.links')
     
